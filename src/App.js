6d4b8173479b58d0e608665a5492cb19f1fb5d77
david hernandez <davidfelipehernandez1@gmail.com>
2022-06-22 12:38:33 -0500

    to-do(#3 banner)

--- src/App.js
+++ src/App.js
@@ -4,30 +4,21 @@ import { TaskCreator } from './components/taskCreator';
 import { TaskTable } from './components/taskTable';
 import { VisibilityControl } from './components/visibilityControl';
 import { Container } from './components/container';
-import { Head } from './components/header/head';
+import { Head } from './components/header/headBanner';
 
 function App() {
 
   const [taskItems, setTasksItems] = useState([]);
   const [showComplete, setShowComplete] = useState(false);
-  const [counter, setCounter] = useState(0);
 
   //Create a new task
   function createNewTask(taskName) {
     if (!taskItems.find(item => item.name === taskName)) {
       setTasksItems([...taskItems, { name: taskName, done: false }]);
-
-      setCounter(counter + 1);
     }
   }
 
-  //Head Status
-  const otro = (taskStatus) => {
-    if (!taskItems.find(item => item.done === taskStatus)) {
-      setCounter(counter - 1);
-    }
-  }
-
+  //toggle
   const toggleTask = (task) => {
     setTasksItems(
       taskItems.map((t) => (t.name === task.name) ? { ...t, done: !t.done } : t)
@@ -46,22 +37,16 @@ function App() {
     if (data) {
       setTasksItems(JSON.parse(data));
     }
-
-    let counterData = localStorage.getItem('active-task')
-    if (counterData) {
-      setCounter(JSON.parse(counterData))
-    }
   }, [])
 
   //Local Storage for new Tasks
   useEffect(() => {
     localStorage.setItem('task', JSON.stringify(taskItems)) //Save the task
-    localStorage.setItem('active-task', JSON.stringify(counter)) //Save the active-tasks
-  }, [taskItems, counter])
+  }, [taskItems])
 
   return (
     <main className="bg-dark vh-100 text-white">
-      <Head counter={counter} />
+      <Head taskItems={taskItems} />
       <Container>
         <TaskCreator createNewTask={createNewTask} />
         <TaskTable
@@ -76,7 +61,7 @@ function App() {
         />
 
         {showComplete === true && (
-          <TaskTable
+          < TaskTable
             task={taskItems}
             toggleTask={toggleTask}
             showComplete={showComplete}
--- src/components/header/headBanner.jsx
+++ src/components/header/headBanner.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export const Head = ({ counter }) => {
+export const Head = ({ taskItems }) => {
   return (
     <header className='bg-secondary p-1 d-flex justify-content-center'>
       <h3 className='mx-3'>Active Tasks</h3>
-      <span className='h3'>{counter}</span>
+      <span className='h3 text-info'>
+        [ {taskItems.filter((t) => !t.done).length} ]
+      </span>
     </header>
   )
 }
